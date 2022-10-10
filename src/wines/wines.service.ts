9b33e213c140223268ad711ef6f0703385e84701
Heloíse Silva Santos <santoshs@taginfraestrutura.com.br>
2022-10-10 19:29:05 -0300

    refactor: Exibição de mensagem de itens não encontrados nos métodos findAll e findOne

--- src/wines/wines.service.ts
+++ src/wines/wines.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Wine } from './entities/wine.entity';
@@ -14,54 +14,38 @@ export class WinesService {
     }
 
     async findAll() {
-        // const allWines = this.wines;
+        const allWines = await this.wineModel.find().exec();
 
-        // if(allWines.length == 0) {
-        //     throw new HttpException(`Nenhum vinho encontrado!`, HttpStatus.NOT_FOUND);
-        // }
+        if(allWines.length == 0) {
+            throw new HttpException(`Nenhum vinho encontrado!`, HttpStatus.NOT_FOUND);
+        }
 
-        // return allWines;
-
-        // indo no BD  e buscando todos os dados que estão na coleção de tarefas
-        return await this.wineModel.find().exec();
+        return allWines;
     }
 
     async findOne(id: string) {
-        // const wine = this.wines.find((Wine) => Wine.id === Number(id));
+        const wine = await this.wineModel.findById(id).exec();
 
-        // if(!wine) {
-        //     throw new HttpException(`Vinho ID #${id} não encontrado!`, HttpStatus.NOT_FOUND);
-        // }
+        if(!wine) {
+            throw new HttpException(`Vinho ID #${id} não encontrado!`, HttpStatus.NOT_FOUND);
+        }
 
-        // return wine;
-
-        return await this.wineModel.findById(id).exec();
+        return wine;
     }
 
     async create(createWineDto: any) {
-        // this.wines.push(createWineDto);
-        // return createWineDto;
-
         const createdWine = new this.wineModel(createWineDto);
+
         return await createdWine.save();
     }
 
     async update(id: string, updateWineDto: any) {
-        // const indexWine = this.wines.findIndex((Wine) => Wine.id === Number(id));
-
-        // this.wines[indexWine] = updateWineDto;
-        
         await this.wineModel.updateOne({ _id: id }, updateWineDto).exec();
+
         return this.findOne(id);
     }
 
     async delete(id: string) {
-        // const indexWine = this.wines.findIndex((Wine) => Wine.id === Number(id));
-
-        // if(indexWine >= 0) {
-        //     this.wines.splice(indexWine, 1);
-        // }
-
         return await this.wineModel.deleteOne({ _id: id }).exec();
     }
 }
