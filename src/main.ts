3b6e0be4101977745f9fd5e2bc1e9d0be16b70f5
Heloise Santos <heloise.hssantos@gmail.com>
2022-11-26 13:46:45 -0300

    habilita cors para qualquer acesso

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*'
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // não deixa ser passada qualquer informação que não seja o que já está estabelecido na classe dto
     forbidNonWhitelisted: true, // devolve mensagem de erro e não permite que seja enviado na requisição qualquer propriedade que não esteja estabelecida na classe dto
